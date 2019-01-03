4026d2a8dbb9af36c0b17e29bffcde0b7326d2fa
Evgeniy <44745673+Jinjik@users.noreply.github.com>
2019-01-03 18:24:26 +0600

    Add files via upload

---
+++ ex.java
@@ -0,0 +1,14 @@
+/* 
+преобразование галлонов в литры
+*/
+
+class ex {
+	public static void main(String args[]) {
+		double litrs;
+		double gallons;
+
+		litrs = 27;
+		gallons = litrs * 3.7854;
+		System.out.println(litrs + " litrs equally " + gallons + " gallons");
+	}
+}
